244e48b278bee58bbc92eecdf45448dc08d9529e
Juan Vilar <juan@hamrodev.com>
2024-04-18 14:18:37 +0200

    added cleanup hook, added mail dns record

---
+++ cleanup_hook.sh
@@ -0,0 +1,6 @@
+sudo chown :mailserver /etc/letsencrypt/live/mail.hamrodev.com/privkey.pem
+sudo chown :mailserver /etc/letsencrypt/live/mail.hamrodev.com/cert.pem
+sudo chmod 640 /etc/letsencrypt/live/mail.hamrodev.com/privkey.pem
+sudo chmod 640 /etc/letsencrypt/live/mail.hamrodev.com/cert.pem
+sudo systemctl restart dovecot
+sudo systemctl restart postfix
