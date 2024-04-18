0aebd8cef140f8959921ee6ae9645062f45e37f5
Juan Vilar <juan@hamrodev.com>
2024-04-18 12:16:55 +0200

    added env variables for certbot

--- hook_script.sh
+++ hook_script.sh
@@ -5,4 +5,4 @@ VALIDATION="$CERTBOT_VALIDATION"
 
 # Add the TXT record to the DNS zone
 echo "Adding TXT record for domain $DOMAIN with value $VALIDATION"
-ts-node "$(dirname "$0")/challenge.ts" $VALIDATION
+ts-node challenge.ts" $VALIDATION
