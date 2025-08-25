79d42c09efe290c84ea7935e27d4840766ee6f79
Juan <troindx@gmail.com>
2025-08-25 16:34:57 +0200

    updated readme

--- pm2.config.js
+++ pm2.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    script: 'index.js',
+    script: './dist/index.js',
     interpreter: 'node',
     exec_mode: 'fork',
     cron_restart: '*/25 * * * *', // Run every 5 minutes
