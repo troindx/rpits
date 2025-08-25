2157427fbc2ae06e3e08400b39fc646ec6b3e896
Juan <troindx@gmail.com>
2025-08-25 18:04:35 +0200

    added autorestart false to pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -2,6 +2,9 @@ module.exports = {
     script: './dist/index.js',
     interpreter: 'node',
     exec_mode: 'fork',
-    cron_restart: '*/25 * * * *', // Run every 5 minutes
+    instances: 1,
+    watch: false,
+    autorestart: false,
+    cron_restart: '*/25 * * * *',
   };
   
